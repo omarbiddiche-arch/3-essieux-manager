046bff349d62eab5f80cb8ded0e128a6be7f78c6
omarbiddiche-arch <omar.biddiche@gmail.com>
2025-12-14 21:11:34 +0100

    Update supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,4 +1,8 @@
 const supabaseUrl = 'https://aivstjuqrqdfohoratwe.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFpdnN0anVxcnFkZm9ob3JhdHdlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjUzNjg4MzAsImV4cCI6MjA4MDk0NDgzMH0.0AebBzzNKmz0ZsqDfctVkLB7WXiFrUrQ_TcD3XcbOt4';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFpdnN0anVxcnFkZm9ob3JhdHdlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjUzNjg4MzAsImV4cCI6MjA4MDk0NDgzMH0.0AebBzzNKmz0ZsqDfctVkLB7WXiFrUrQ_TcD3XcbOt4';
 
-const supabase = window.supabase.createClient(supabaseUrl, supabaseAnonKey);
+// Initialize and expose globally
+window.supabaseClient = window.supabase.createClient(supabaseUrl, supabaseKey);
+
+console.log("Supabase client initialized", window.supabaseClient)
+
